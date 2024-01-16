c9013937b7ccb64a3440cf8a4749237f1caef9d4
Cindy Joan <joancindi535@gmail.com>
2024-01-16 16:12:25 +0300

    sign up page

---
+++ src/Components/LogoLight.js
@@ -0,0 +1,14 @@
+ import React from "react";
+
+ const LogoLight = () =>{
+    return(
+        <>
+            <div className="logolight">
+            <p className="logolight1">Story</p>
+            <p className="logolight2">Recs</p>
+            </div>
+        </>
+    )
+ }
+
+ export default LogoLight;
--- src/Onboarding/Signup.js
+++ src/Onboarding/Signup.js
@@ -1,20 +1,40 @@
 import React from "react";
-import "../Onboarding/onboarding.css"
-import illustration from "../Images/Audiobook.svg"
+import "../Onboarding/onboarding.css";
+import illustration from "../Images/Audiobook.svg";
+import LogoLight from "../Components/LogoLight";
 
-const Signup = () =>{
-    return(
-        <>
-          <div className="signup">
-           <div className="signup-illustration">
-             <img src={illustration} alt="illustration"/>
-           </div>
-           <div className="signup-form">
-            <h3>Welcome to StoryRecs!</h3>
-           </div>
+const Signup = () => {
+  return (
+    <>
+      <div className="signup">
+        <div className="signup-illustration">
+          <LogoLight />
+          <img src={illustration} alt="illustration" />
+        </div>
+        <div className="signup-form">
+          <h3>Welcome to StoryRecs!</h3>
+          <div className="signup-inputs">
+            <div className="signup-input">
+              <input type="text" placeholder="First Name"/>
+            </div>
+            <div className="signup-input">
+              <input type="text" placeholder="Last Name"/>
+            </div>
+            <div className="signup-input">
+              <input type="text" placeholder="Email"/>
+            </div>
+            <div className="signup-input">
+              <input type="password" placeholder="Password"/>
+            </div>
+            <div className="signup-input">
+              <input type="password" placeholder="Confirm Password"/>
+            </div>
+            <button>Sign up</button>
           </div>
-        </>
-    )
-}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Signup;
