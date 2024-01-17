25a5ff1ccf097e9336b5da231f4de130a37bf6e5
Cindy Joan <joancindi535@gmail.com>
2024-01-17 14:56:34 +0300

    Sign up page design

--- src/Onboarding/Signup.js
+++ src/Onboarding/Signup.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../Onboarding/onboarding.css";
-import illustration from "../Images/Audiobook.svg";
+import illustration from "../Images/Glasses.svg";
 import LogoLight from "../Components/LogoLight";
+import Google from "../Images/google.svg"
+
 
 const Signup = () => {
   return (
@@ -13,6 +16,7 @@ const Signup = () => {
         </div>
         <div className="signup-form">
           <h3>Welcome to StoryRecs!</h3>
+          <p className="signup-desc">Create an account to get accurate book recommendations based on your favorite books or recent reads.</p>
           <div className="signup-inputs">
             <div className="signup-input">
               <input type="text" placeholder="First Name"/>
@@ -24,12 +28,26 @@ const Signup = () => {
               <input type="text" placeholder="Email"/>
             </div>
             <div className="signup-input">
-              <input type="password" placeholder="Password"/>
+              <input type="password" placeholder="Password" autoComplete="off"/>
             </div>
             <div className="signup-input">
-              <input type="password" placeholder="Confirm Password"/>
+              <input type="password" placeholder="Confirm Password" autoComplete="off"/>
+            </div>
+            <div className="terms-and-conditions">
+              <input type="checkbox" className="checkbox"/>
+              <div className="policy-text">
+                <p>I agree with StoryRec's</p>
+                <Link path to="" className="privacy-link">Privacy Policy</Link>
+                <p>and</p>
+                <Link path to="" className="privacy-link">Terms of Use</Link>
+              </div>
             </div>
             <button>Sign up</button>
+            <p className="or">Or</p>
+            <div className="google">
+             <img src={Google} alt="google"/>
+             <p>Sign up with Google</p>
+            </div>
           </div>
         </div>
       </div>
